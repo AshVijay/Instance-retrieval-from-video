1f608f5e54f2161ba015ae6dac601067b92c0fdc
ashwinvijay19 <ashwinvijay19@yahoo.com>
2019-03-31 11:01:59 -0400

    implemented Edgeboxes

---
+++ Code/README.md
@@ -0,0 +1,43 @@
+## This is a python implementation of a reconstruction model based object search from video sources
+
+
+
+The codebase structure is as follows:
+
+
+   |-------data-preprocesser---|
+   |                           |
+   |                           |
+   |
+   |
+   |-------data-loader---------|
+   |                           |
+   |                           |
+   |
+   |
+   |-------model---------------|
+   |                           |
+   |                           |
+   |
+   |
+   |-------learner-------------|
+   |                           |
+   |                           |
+   |
+   |
+   |-------utils---------------|
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |
+   |-------README.md
+   |
+   |-------config.json
+   |
+   |-------requirements.txt
+   |
+   |-------setup.py
+   |
+   |-------main.py
+
---
+++ Code/data-handler/edgeBoxes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+This sample demonstrates structured edge detection and edgeboxes.
+Usage:
+  edgeboxes_demo.py [<model>] [<input_image>]
+'''
+
+import cv2 as cv
+import numpy as np
+import sys
+import os
+img_path = os.path.abspath("home/ashwin/Desktop/Semester 3/Object Retrieval from Video/Implementation/Datasets/CNN2h Dataset/temp/Frame000004.jpg")
+if __name__ == '__main__':
+    print(__doc__)
+
+    model = sys.argv[1]
+    im = cv.imread("Frame000027.jpg")
+
+    edge_detection = cv.ximgproc.createStructuredEdgeDetection(model)
+    rgb_im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    edges = edge_detection.detectEdges(np.float32(rgb_im) / 255.0)
+
+    orimap = edge_detection.computeOrientation(edges)
+    edges = edge_detection.edgesNms(edges, orimap)
+
+    edge_boxes = cv.ximgproc.createEdgeBoxes()
+    edge_boxes.setMaxBoxes(10)
+    boxes = edge_boxes.getBoundingBoxes(edges, orimap)
+
+    for b in boxes:
+        x, y, w, h = b
+        cv.rectangle(im, (x, y), (x+w, y+h), (0, 255, 0), 1, cv.LINE_AA)
+
+    cv.imshow("edges", edges)
+    cv.imshow("edgeboxes", im)
+    cv.waitKey(5000)
+    cv.destroyAllWindows()
+   
+
+
+
+    
+ 
